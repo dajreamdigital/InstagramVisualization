2fd45d3041dfa1e7f7052e6dc60a4da3f83b483c
vedanujg <vedanujg@gmail.com>
2016-04-01 23:32:51 -0400

    Modify download script to spread media over various months

--- scripts/DownloadMediaByCity.py
+++ scripts/DownloadMediaByCity.py
@@ -5,25 +5,28 @@ base_url = 'https://api.instagram.com/v1'
 ACCESS_TOKEN = 'Use your own'
 
 #Atlanta
-lat = '33.748995'
-lng = '-84.387982'
+lat = '38.907192'
+lng = '-77.036871'
 
-f_image = open('Atlanta.csv','w')
+f_image = open('Washington.csv','w')
 
 writer_image = csv.writer(f_image)
 
 writer_image.writerow([ 'id', 'link', 'tags','filter', 'comments', 'likes', 'latitude', 'longitude',\
- 'locationname', 'locationid', 'createdtime', 'imageurl', 'username', 'realname'])
+ 'locationname', 'locationid', 'createdtime', 'imageurl', 'userid', 'username', 'realname'])
 
 max_timestamp = int(time.time())
 print max_timestamp
 
-for i in range(5000):
+month_count = 0
+
+for i in range(6000):
 	try:
 		url = '{0}/media/search?lat={1}&lng={2}&max_timestamp={3}&count=100&access_token={4}'.format(base_url, lat, lng, str(max_timestamp), ACCESS_TOKEN)
 		r = requests.get(url)
 		print url
 		if r.status_code == 200:
+            month_count = month_count + 1
 			rjson = r.json()
 			datalist = rjson['data']
 			print 'Got %d images in call %d'%(len(datalist), i)
@@ -43,6 +46,7 @@ for i in range(5000):
 					data['location']['id'] if data['location'] else 'None',\
 					data['created_time'],\
 					data['images']['standard_resolution']['url'].split('.jpg', 1)[0] + '.jpg',\
+                    data['user']['id'],\
 					data['user']['username'],\
 					data['user']['full_name'],\
 					]
@@ -52,6 +56,9 @@ for i in range(5000):
 					print 'Error parsing image data'
 					print e
 			max_timestamp = datalist[-1]['created_time']
+            if(month_count == 500):
+                max_timestamp = max_timestamp - 2629743
+                month_count = 0
 			print 'max_timestamp is now ',max_timestamp
 			time.sleep(1)
 		else:
@@ -59,7 +66,7 @@ for i in range(5000):
 	except Exception as ex:
 		print 'Error getting url'
 		print ex
-	
+
 	#time.sleep(0.7)
 
 f_image.close()
