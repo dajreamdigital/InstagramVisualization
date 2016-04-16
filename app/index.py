bfb089e04675cf6852058ceb31b23033ded0c2a8
vedanujg <vedanujg@gmail.com>
2016-04-15 22:56:03 -0400

    Add Flask App. Initial Commit.

---
+++ app/index.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template,request,json
+app = Flask(__name__,static_url_path='/static')
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+
+
+
+# @app.route("/autocomplete")
+# def autocomplete():
+#     return render_template('autocomplete.html')
+#
+# @app.route('/ajaxautocomplete',methods=['POST', 'GET'])
+# def ajaxautocomplete():
+#     result=''
+#     if request.method=='POST':
+#      query=request.form['query']
+#
+#      try:
+#
+#       with connection.cursor() as cursor:
+# 	      sql="select country_code data,country_name value from  countries where country_name like '%"+query+"%'"
+# 	      cursor.execute(sql)
+# 	      result = cursor.fetchall()
+#      finally:
+#       a=2
+#      return json.dumps({"suggestions":result})
+#     else:
+#
+#      return "ooops"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
