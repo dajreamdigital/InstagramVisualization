820a524c48b900b854778321e53739b4f0961f89
vedanujg <vedanujg@gmail.com>
2016-04-18 03:19:19 -0400

    Add recommend call via Ajax and show buttons on button click.

--- app/index.py
+++ app/index.py
@@ -9,30 +9,11 @@ def hello():
 def visualize():
     return render_template('visualize.html')
 
+@app.route('/recommend')
+def recommend():
 
-# @app.route("/autocomplete")
-# def autocomplete():
-#     return render_template('autocomplete.html')
-#
-# @app.route('/ajaxautocomplete',methods=['POST', 'GET'])
-# def ajaxautocomplete():
-#     result=''
-#     if request.method=='POST':
-#      query=request.form['query']
-#
-#      try:
-#
-#       with connection.cursor() as cursor:
-# 	      sql="select country_code data,country_name value from  countries where country_name like '%"+query+"%'"
-# 	      cursor.execute(sql)
-# 	      result = cursor.fetchall()
-#      finally:
-#       a=2
-#      return json.dumps({"suggestions":result})
-#     else:
-#
-#      return "ooops"
-
+    result = ['vintage', 'lomo', 'clarity','sunrise']
+    return json.dumps({"suggestions":result})
 
 if __name__ == "__main__":
     app.run(debug=True)
